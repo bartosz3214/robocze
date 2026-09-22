0facfdb54d1748be040e70d981c9b69a16d8606d
bartosz3214 <bartoszkornak13@proton.me>
2026-09-22 14:36:57 +0200

    zadanie 2

--- src/index.php
+++ src/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <ti
+    tle>Document</title>
 </head>
 <body>
-    php
+    git branch --show-current
 <?php
 echo "Hello, world!";
 ?>
